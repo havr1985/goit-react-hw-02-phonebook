d258cddadf4ce120036c08bc809962fadac0ab85
havr1985 <gavrtel1985@gmail.com>
2023-10-15 21:30:34 +0300

    1

--- src/components/FormContact/FormContact.js
+++ src/components/FormContact/FormContact.js
@@ -1,4 +1,15 @@
 import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+
+const contactsSchema = Yup.object().shape({
+    name: Yup.string
+        .matches(/^[a-zA-Z\s]+$/, 'Please, enter only letter')
+        .min(3, 'Too short!')
+        .required('This field is required!'),
+    number: Yup.string
+        .matches(/^\d{3}-\d{2}-\d{2}$/, 'Format: XXX-XX-XX')
+        .required('This field is required!'),
+})
 
 export const FormContact = ({ onAdd }) => {
     return (
@@ -8,19 +19,23 @@ export const FormContact = ({ onAdd }) => {
                     name: '',
                     number:'',
         
-                }}
+            }}
+            validationSchema={contactsSchema}
                 onSubmit={(values, actions) => {
                     onAdd(values);
                     actions.resetForm();
-                }}
-            >
+            }}>
+            
+
+            {({errors}) => (
                 <Form>
                     <label>Name
-                        <Field name="name" placeholder="Enter name" required/>
+                    <Field name="name" type="text" placeholder="Enter name" />
+                    <div>{errors.name}</div>
                     </label>
 
                     <label>Number
-                        <Field name="number" placeholder="Enter number" required />
+                        <Field name="number" type="number" placeholder="Enter number"/>
                     </label>
 
                     <button type="submit">Submit</button>
